a89049851aa3c3f7ad086f66860e0fbef5640524
Kunal Mishra <kmishra9@berkeley.edu>
2019-01-14 03:10:58 -0800

    Reformatted main

--- main.tf
+++ main.tf
@@ -1,12 +1,6 @@
 /* TODO:
 
-- Build IAM users (researchers + administrators)
-- Build IAM groups (researchers + log analysis + administrators)
-- IAM Password Policy
 - Security Token Service Regions (Deactivate pretty much everything except for US West (Oregon))
-- Log Analysis (optional)
-- Creating a bunch of customer-managed policies (filter by customer-managed in KaiserFlu to check)
-
 - Adding provisioners for iptable setup, ssh tunnel creation, users being added (docs: https://www.terraform.io/docs/provisioners/index.html)
   - The file provisioner in particular would be a nice way to port scripts from the github repo to the instance
   - local-exec will be useful once scripts have been deposited in the appropriate -- you can interpolate resource variables (such as VPC ips, which should be useful for iptables setup)
@@ -27,31 +21,33 @@ provider "aws" {
 # Configuring CloudTrail
 
 module "cloudtrail" {
-  source = "./cloudtrail"
-  project_name = "${var.project_name}"
+  source            = "./cloudtrail"
+  project_name      = "${var.project_name}"
   organization_name = "${var.organization_name}"
-  log_export = "${var.log_export}"
-  region = "${local.region}"
-  stage = "${local.stage}"
+  log_export        = "${var.log_export}"
+  region            = "${local.region}"
+  stage             = "${local.stage}"
 }
 
 ################################################################################
 # Configuring IAM users, roles, groups, and privileges
 
 module "IAM" {
-  source = "./IAM"
-  project_name = "${var.project_name}"
+  source          = "./IAM"
+  project_name    = "${var.project_name}"
   num_researchers = "${var.num_researchers}"
-  num_admins = "${var.num_admins}"
-  log_export = "${var.log_export}"
+  num_admins      = "${var.num_admins}"
+  log_export      = "${var.log_export}"
 }
 
 ################################################################################
 # Configuring network and firewall
 
-# VPC Module: https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/1.50.0
-
-# Security Group Module: https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/2.10.0
+module "VPC" {
+  source         = "./VPC"
+  project_name   = "${var.project_name}"
+  flow_logs_role = "${module.IAM.FlowLogsRole}"
+}
 
 ################################################################################
 # Configuring the analysis instance
