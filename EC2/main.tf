13b996ca8affab7a9f64f501f07a57ff1bb1963c
Kunal Mishra <kmishra9@berkeley.edu>
2019-01-16 01:55:54 -0800

    Additional progress on EC2

--- EC2/main.tf
+++ EC2/main.tf
@@ -12,18 +12,53 @@ data "aws_ami" "cis_level_1_ami" {
 ################################################################################
 # EC2 Instance Setup
 resource "aws_instance" "EC2_analysis_instance" {
-  ami           = "${data.aws_ami.cis_level_1_ami.id}"
-  availability_zone = "${var.region}${var.availability_zone}"
-  tenancy = "default"
-  disable_api_termination = "true"
+  ami                                  = "${data.aws_ami.cis_level_1_ami.id}"
+  availability_zone                    = "${var.region}${var.availability_zone}"
+  tenancy                              = "default"
+  disable_api_termination              = "true"
   instance_initiated_shutdown_behavior = "stop"
-  instance_type = "${var.instance_type}"
-  monitoring = "true"
-  vpc_security_group_ids = "${var.security_group_ids}"
+  instance_type                        = "${var.instance_type}"
+  monitoring                           = "true"
+  vpc_security_group_ids               = "${var.security_group_ids}"
+  iam_instance_profile                 = "${var.CloudWatchLogsRole.name}"
 
   tags = {
     Name = "${var.project_name}_EC2_analysis_instance"
   }
+
+  ##############################################################################
+  # EBS volumes attached to the EC2 resource
+  root_block_device = {
+    volume_type = "standard"
+    volume_size = "${var.root_volume_size}"
+    delete_on_termination = "false"
+  }
+
+  ebs_block_device = {
+    device_name = "${var.project_name}_data"
+    volume_type = "standard"
+    volume_size = "${var.EBS_volume_size}"
+    delete_on_termination = "false"
+    encrypted = "true"
+  }
+
+  ##############################################################################
+  # Commands to run on creation of the EC2 resource
+
+  # TODO: Need to determine whether it's possible to connect to a cis ami through terraform like this (or how to at all)
+
+  # Add Swap
+  provisioner "local-exec"{
+    command = ""
+    working_dir = "~/"
+  }
+
+  # Mount Drives
+  provisioner "local-exec" "mount_drives" {
+    command = ""
+    working_dir = "~/"
+  }
+
 }
 
 
